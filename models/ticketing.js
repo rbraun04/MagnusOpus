bf312e7279a6b39dc3e3237eaac391453caa792c
Richard Antolin <richardantolin@gmail.com>
2020-09-08 14:58:09 -0700

    updated files

---
+++ models/ticketing.js
@@ -0,0 +1,34 @@
+module.exports = (sequelize, DataTypes) => {
+    const Tickets = sequelize.define("ticketing", {
+        name: {
+            menuSelection: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            customerName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            customerEmail: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            customerlicenseID: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            carMake: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            carModel: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+        }
+    })
+    Tickets.associate = function(models){
+        Tickets.hasOne(models.Vendor)
+    }
+    return Tickets;
+}

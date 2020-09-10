201743b803d0b254e56abe559fdd6f6ea6d0dd40
Ryan Braun <ryan@thelyricla.com>
2020-09-10 10:16:16 -0700

    merge conflicts

---
+++ models/waitlist.js
@@ -0,0 +1,39 @@
+module.exports = (sequelize, DataTypes) => {
+    const Waitlist = sequelize.define("waitlist", {
+            menuSelection: {
+                type: DataTypes.STRING,
+                allowNull: true
+            },
+            customerName: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            customerEmail: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            customerlicenseID: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            carMake: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            carModel: {
+                type: DataTypes.STRING,
+                allowNull: false
+            },
+            reservationNumber: {
+                type: DataTypes.STRING,
+                allowNull: true
+            }
+    })
+    Waitlist.associate = function(models){
+        console.log(models)
+        Waitlist.hasMany(models.vendor, {
+            onDelete:"cascade"
+        })
+    }
+    return Waitlist;
+}

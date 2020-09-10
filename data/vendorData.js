4a5b7104c9672cc44c355608b91af78fad86cdf5
Richard Antolin <richardantolin@gmail.com>
2020-09-10 11:25:18 -0700

    added vendor seed files

---
+++ data/vendorData.js
@@ -0,0 +1,12 @@
+var  vendorArray = [
+    {
+      name: "Kids Junk Food",
+      description: "Yummy Stuff",
+      option1: "french fries",
+      option2: "macaroni and cheese",
+      option3: "tuna sandwich",
+      option4: "fruits",
+      option5: "chicken nuggets",
+    },
+  ];
+  module.exports = vendorArray;

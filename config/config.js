feff20306a4e25752aa7ff6326dbcbf2f83c79aa
Ryan Braun <ryan@thelyricla.com>
2020-09-11 14:59:01 -0700

    heroku

--- config/config.js
+++ config/config.js
@@ -15,10 +15,7 @@ module.exports = {
     dialect: "mysql"
   },
   production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
+    "use_env_variable": "JAWSDB_URL",
+    "dialect": "mysql"
   }
 }
